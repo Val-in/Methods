310edd8db6496c30935bea40344c2e9a37099003
Val-in <valeriia.vp1@yandex.ru>
2024-04-27 12:54:43 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Val";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
