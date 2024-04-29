16e60a7c5aabf16133fc5106c76e1228fe755e35
Val-in <valeriia.vp1@yandex.ru>
2024-04-29 19:00:52 +0800

    Первая версия программы

--- Program.cs
+++ Program.cs
@@ -4,9 +4,15 @@
     {
         static void Main(string[] args)
         {
-            string MyName;
-            MyName = "Val";
+            const string MyName = "Val";
+
             Console.WriteLine(MyName);
+            Console.WriteLine("\t Hello World");
+            Console.WriteLine("\t Мне 26 лет");
+            Console.WriteLine("\t My name is \n {0}", MyName);
+
+            Console.WriteLine('\u0040');
+            Console.WriteLine('\u0023');
             Console.ReadKey();
         }
     }
